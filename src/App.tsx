459bd17a9a41a76d3e7d7be42baf9bc2e6a1a409
ygmurrsimsek <ygmurrsimsek@icloud.com>
2025-05-08 22:49:17 +0300

    Proje tamamlandı.
    Diemonsions ile ekran boyutunun ölçüldüğünü,ListHeaderComponent kullanımını öğrendim ve uyguladım .

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { SafeAreaView,Text,View,TouchableOpacity ,StyleSheet,FlatList,ScrollView,Image} from "react-native";
+import { SafeAreaView,Text,View,TouchableOpacity ,StyleSheet,FlatList,ScrollView,Image,Dimensions} from "react-native";
 import newsData from "./data/news_data.json";
 import NewsCard from "./Components/NewsCard/NewsCard.jsx";
 import newsBannerData from "./data/news_banner_data.json";
@@ -15,21 +15,19 @@ const News = () => {
   console.log(newsData);
   return(
     <SafeAreaView style={style.container}>
-      <View>
-        <ScrollView>
-          {newsBannerData.map(banner => (
-            console.log(banner.id),
-            console.log(banner.imageUrl),
-
-            <Image 
-              key={banner.id} 
-              source={{ uri: banner.imageUrl }} 
-            />
-          ))}
-        </ScrollView>
+        <Text style={style.headerText}>News</Text>
         <FlatList data={newsData} renderItem={RenderNews} 
-          keyExtractor={(item)=> item.u_id.toString()} />
-      </View>
+          keyExtractor={(item)=> item.u_id.toString()} 
+          ListHeaderComponent={()=>(
+            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            {newsBannerData.map(banner => (
+              <Image style={style.image}
+                key={banner.id} 
+                source={{ uri: banner.imageUrl}} 
+              />
+            ))}
+          </ScrollView>
+          )}/>
     </SafeAreaView>
    
   );
@@ -39,6 +37,15 @@ const style=StyleSheet.create({
   container:{
     flex:1,
     backgroundColor:"#f0f8ff",
+  },
+  image:{
+    height:Dimensions.get('window').height/5,
+    width:Dimensions.get('window').width/2,
+  },
+  headerText:{
+    fontSize:30,
+    fontWeight:"bold",
+    marginLeft:10,
   }
 }
 );
